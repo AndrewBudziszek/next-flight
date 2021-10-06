7adf2524128ccc445b1435816ec3570665fddfc7
Andrew Budziszek <andrew@budziszek.com>
2021-10-06 10:46:17 -0500

    setup vercel adapter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,12 @@
-module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.svelte'],
-}
+import vercel from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: vercel()
+	}
+};
+
+export default config;
