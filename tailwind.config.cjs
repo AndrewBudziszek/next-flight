4abf7c8208e1ce3a88d8fb14d5f7b9da09b768d1
Andrew Budziszek <andrew@budziszek.com>
2021-10-06 10:49:17 -0500

    updated wrong config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
-	}
-};
-
-export default config;
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.svelte'],
+}
