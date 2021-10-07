8f84a6ce6268f69fa025e9f60265845f2f6019f4
Andrew Budziszek <andrew@budziszek.com>
2021-10-07 10:29:28 -0500

    google ads fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,6 @@
 <script>
     import Nav from './nav.svelte'
 </script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2340680669187881"
-     crossorigin="anonymous"></script>
 
 <div class='p-8 mx-auto min-h-screen bg-gradient-to-b from-gray-900 to-gray-800'>
     <Nav />
