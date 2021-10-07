fdbc6090a90cf8dbd4ddf06ce8e87bb77a0092c9
Andrew Budziszek <andrew@budziszek.com>
2021-10-07 10:56:06 -0500

    trying to add google adsense

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,12 @@
 <script>
     import Nav from './nav.svelte'
+    import Ads from './ads.svelte'
 </script>
 
 <div class='p-8 mx-auto min-h-screen bg-gradient-to-b from-gray-900 to-gray-800'>
     <Nav />
     <slot />
+    <Ads />
 </div>
 
 <style>
---
+++ src/routes/ads.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {onMount} from 'svelte'
+    onMount(() => {
+   (window.adsbygoogle = window.adsbygoogle || []).push({});
+ });
+</script>
+
+<div class="ads-widget-container">
+<!-- next-flight -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2340680669187881"
+     data-ad-slot="8873106052"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+</div>
