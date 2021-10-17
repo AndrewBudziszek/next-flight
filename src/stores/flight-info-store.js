fee7b56403fb9b3d7ecb334cde2453dc17a03ed5
Andrew Budziszek <andrew@budziszek.com>
2021-10-17 08:31:50 -0500

    trying a new thing for vercel.

--- src/stores/flight-info-store.js
+++ src/stores/flight-info-store.js
@@ -40,10 +40,10 @@ export const fetchFlights = async (airportCode) => {
 async function addPriceDataToFlight(flights) {
     for(var i = 0; i < flights.length; i++) {
         let flight = flights[i]; 
-        let origin = get(stationCode).substr(1); // origin in icao format
+        let flight_origin = get(stationCode).substr(1); // origin in icao format
         let destination = flight.arrival.airport.icao.substr(1);
         let depart_date = moment(flight.departure.scheduledTimeLocal).format('YYYY-MM');
-        const baseURL = `https://flight-data-api.herokuapp.com/price?origin=${origin}&destination=${destination}&depart_date=${depart_date}`;
+        const baseURL = `https://flight-data-api.herokuapp.com/price?origin=${flight_origin}&destination=${destination}&depart_date=${depart_date}`;
         
 
         let res = await axios.get(baseURL);
